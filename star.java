6107cbee99c687aaaf699f97ad10b7b5d308e0a8
Pravin-132005 <23124042@srcas.ac.in>
2025-01-25 12:43:09 +0530

    push

---
+++ star.java
@@ -0,0 +1,18 @@
+class star
+	{
+		public static void main(String args[])
+			{
+				int x=6;
+				int i;
+				int j;
+				for(i=1;i<=x;i++)
+					{
+						for(j=1;j<=i;j++)
+							{
+								System.out.print("  *  ");
+								System.out.print("");
+							}
+						System.out.println();
+					}
+			}	
+	}
